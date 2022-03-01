7a32bcb44d75f814a96fdd3831b6b8e606b6b54e
Chibueze Kalu <kalu.chibueze@hotmail.com>
2022-03-01 14:00:44 +0100

    completed state management context API

--- src/screen/AuthenticationScreen/index.js
+++ src/screen/AuthenticationScreen/index.js
@@ -1,14 +1,17 @@
-import { useNavigation, useContext } from "@react-navigation/native";
-import React from "react";
+import { useNavigation } from "@react-navigation/native";
+
+import React, { useContext } from "react";
 import { View, Text, Button } from "react-native";
 import { Context } from "../../hooks/Context/AuthContext";
 
 import { styles } from "./styles";
 
 const AuthenticationScreen = ({ navigation }) => {
+  const { state } = useContext(Context);
+
   return (
     <View>
-      <Text>Welcome back </Text>
+      <Text>Welcome back {state[0].name}</Text>
       <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
     </View>
   );
